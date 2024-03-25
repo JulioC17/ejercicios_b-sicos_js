6505e557f11e34de1ddb62f1423c535c45cbb9bc
JulioC17 <159253136+JulioC17@users.noreply.github.com>
2024-03-25 17:48:14 +0100

    ejercicio 3 y 4 terminado

---
+++ ejercicio#4/ejercicio#4.js
@@ -0,0 +1,39 @@
+const aldeanos = ["Fibrilio", "Narciso", "Vacarena", "Tendo", "Nendo"];
+//4.1 - Saca a "Tendo" por consola atacando su posición.
+console.log(aldeanos[3])
+
+
+
+//4.2 - Coloca en el último lugar de este array a "Cervasio".
+aldeanos.push("Cervasio")
+
+
+
+
+//4.3 - Cambia el primer elemento de este array por "Bambina".
+aldeanos.splice(0, 1, "Bambina")
+
+
+
+
+//4.4 - Dale la vuelta a este array.
+aldeanos.reverse()
+
+
+
+
+
+//4.5 - Cambia a "Narciso" por "Canela" haciendo uso de un método de array.
+aldeanos.splice(1, 1, "Canela");
+
+
+
+
+
+//4.6 - Imprime por consola el último elemento de este array sin atacar a la posición explicitamente
+// Pista: puedes usar el método length con algo más para ello.
+for (let i = 4; i < aldeanos.length; i++){
+    
+    console.log(aldeanos[i])
+
+}
